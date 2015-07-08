053fc74f22ea1859c8c323b464a1a607bd443b84
anoukruhaak <anoukruhaak@gmail.com>
2015-07-08 13:20:55 +0200

    moving files into dirs

--- Javascript/debug-challenge.js
+++ Javascript/debug-challenge.js
@@ -2,7 +2,6 @@ var people = {people: [{name: "Anna", age: null}, {name: "Kim", age: 18}, {name:
 var clean_people = [];
 
 //Only include those people where age is given
-
 people.forEach(function(person){
 	if(person.age instanceof Number){
 		clean_people.push(person)
