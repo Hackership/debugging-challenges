7085af928036553292381ab88664e6724490458d
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-07-08 15:09:54 +0200

    prop up challenge

--- Python/debug-challenge.py
+++ Python/debug-challenge.py
@@ -1,6 +1,18 @@
-stuff = {"name": "Charles", "age": 16, "in_school": True, "has_humor": False, "can_vote": False, "pet_name":"Boris", "best_friend": "Anna", "number_of_pets": 3}
+from pprint import pprint
+
+stuff = {"name": "Charles",
+         "age": 16,
+         "in_school": True,
+         "has_humor": False,
+         "can_vote": False,
+         "pet_name": "Boris",
+         "best_friend": "Anna",
+         "number_of_pets": 3}
 
 result = []
+
+# import pdb
+# pdb.set_trace()
 
 for key, value in stuff.iteritems():
     if isinstance(value, int):
@@ -10,5 +22,4 @@ for key, value in stuff.iteritems():
     elif isinstance(value, bool):
         result.append(dict(name=key, type='bool',
                            value=value and 'true' or 'false'))
-print result
-
+pprint(result)
