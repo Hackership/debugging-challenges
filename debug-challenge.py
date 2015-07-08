ca260db15bfedda82aa5036324c74eacaf9888c3
anoukruhaak <anoukruhaak@gmail.com>
2015-07-08 12:58:47 +0200

    debugged the python challenge - lovely irony

--- debug-challenge.py
+++ debug-challenge.py
@@ -1,8 +1,8 @@
-items = {name: "Charles", age: 16, in_school: true, has_humor: false, can_vote: false, pet_name:"Boris", best_friend: "Anna", number_pets: 3}
+stuff = {"name": "Charles", "age": 16, "in_school": True, "has_humor": False, "can_vote": False, "pet_name":"Boris", "best_friend": "Anna", "number_of_pets": 3}
 
 result = []
 
-for key, value in items:
+for key, value in stuff.iteritems():
     if isinstance(value, int):
         result.append(dict(name=key, value=value, type='int'))
     elif isinstance(value, float):
