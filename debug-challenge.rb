906281b8a93ab5bad332089af6eafb36f2ea10ea
anoukruhaak <anoukruhaak@gmail.com>
2015-07-08 12:50:27 +0200

    fixed ruby debug challenge

--- debug-challenge.rb
+++ debug-challenge.rb
@@ -1,10 +1,12 @@
-people = {people: [{name: "Anna", age: 8}, {name: "Kim", age: 18}, {name: "Peter", age: nil}, {name: "Jonathan", age: 26}, {name: "Lola", age: nil}, {name: "Hank", age: 12}, {name: "Casper", age:23}]}
+people = [{name: "Anna", age: 8}, {name: "Kim", age: 18}, {name: "Peter", age: nil}, {name: "Jonathan", age: 26}, {name: "Lola", age: nil}, {name: "Hank", age: 12}, {name: "Casper", age:23}]
 clean_people = []
 
 #Only include those people where age is given
 people.each do |person|
-  clean_people.push(person) if person["age"]
+  if person["age"].kind_of? Object
+   clean_people.push(person)
+  end
 end
 
+puts(clean_people)
 
-puts clean_people
